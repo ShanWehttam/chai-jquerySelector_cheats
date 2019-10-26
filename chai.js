b14cb5050b946361a5991499a5deaa8be74276bd
ShanWehttam <mnash@hotmail.co.uk>
2019-10-26 21:44:47 +0100

    Update chai.js

--- chai.js
+++ chai.js
@@ -1,5 +1,5 @@
-  expect( ({ a: { c: 3, d: 4 }} ).a, "Must include this key").to.include.key('d'); // passes
-  expect( { c: 3, d: 4 }, "Must include all these keys").to.include.keys('d', 'c'); // passes
-  expect( { c: 3, d: 4 }, "Must include all these keys").to.include.any.keys('d', 'c', 'e', 'f'); // passes
-  expect([1, 2, 3]).to.include.members([1,2,3]);
-  expect([1, 2, 3]).to.have.lengthOf(3).and.include.members([1,2,3]);
+expect( ({ a: { c: 3, d: 4 }} ).a, "Must include this key").to.include.key('d')
+expect( { c: 3, d: 4 }, "Must include all these keys").to.include.keys('d', 'c')
+expect( { c: 3, d: 4 }, "Can include any one of these").to.include.any.keys('d', 'c', 'e', 'f')
+expect([1, 2, 3]).to.include.members([1,2,3])
+expect([1, 2, 3]).to.have.lengthOf(3).and.include.members([1,2,3])
