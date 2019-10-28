18d8ce9a159d340b75ffb1a9af75993695faa861
ShanWehttam <mnash@hotmail.co.uk>
2019-10-28 14:31:58 +0000

    Update chai.js

--- chai.js
+++ chai.js
@@ -3,6 +3,8 @@ expect( { c: 3, d: 4 }, "Must include all these keys").to.include.keys('d', 'c')
 expect( { c: 3, d: 4 }, "Can include any one of these").to.include.any.keys('d', 'c', 'e', 'f')
 expect([1, 2, 3]).to.include.members([1,2,3,3,3,2])
 expect([1, 2, 3]).to.have.lengthOf(3).and.include.members([1,2])
+ expect([1, 2, 3]).to.have.length(3); // Recommended
+expect('foo').to.have.lengthOf(3); // Recommended
 expect([{a: 1}, {b: 2}, {c: 3}]).to.deep.include.members([{a: 1}, {b: 2}])
   expect([{a: 1}, {b: 2}]).to.deep.include({a: 1})
   expect({'.a': {'[b]': 'x'}}, "F correct").to.deep.include.property('\\.a.\\[b\\]', 'x');
@@ -16,3 +18,13 @@ expect({a: 1}).to.include.property('a', 1);
 
 expect({a: 2, b: 3}).to.eql({a: 2, b: 3})
 expect(1.5).to.eql(1.5)
+expect({a: 1}).to.eql({a: 1}); // Recommended
+//cy.log('.ordered is not working')
+//cy.log('Sets and Maps are not supported')
+//cy.log(".nested doesnt work!!!")
+//cy.log("use .property over .include!!! .include looks for property anyway")
+ expect([]).to.be.empty;
+ expect('').to.be.empty;
+ expect({}).to.be.empty;
+// to.eql wraps deep, so use .eql
+//expect({a: 1}).to.deep.equal({a: 1}); // without the 'deep', it would treat it as {a: 1} === {a: 1} (strict equality) which is false
